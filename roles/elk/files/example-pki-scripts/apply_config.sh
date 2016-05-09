77ab64269fa30654b14480ced9d700408fe21fe0
dsvetlov <daniil@svetlov.org>
2016-05-09 19:05:02 +0300

    Fix SG

--- roles/elk/files/example-pki-scripts/apply_config.sh
+++ roles/elk/files/example-pki-scripts/apply_config.sh
@@ -2,8 +2,8 @@
 set -e
 /usr/share/elasticsearch/plugins/search-guard-2/tools/sgadmin.sh \
    -cd /usr/share/elasticsearch/plugins/search-guard-2/sgconfig/ \
-   -ks /etc/elasticsearch/node-{{node_name}}-keystore.jks \
-   -ts /etc/elasticsearch/truststore.jks \
+   -ks /etc/elasticsearch/ssl/spock-keystore.jks \
+   -ts /etc/elasticsearch/ssl/truststore.jks \
    -kspass changeit \
    -tspass capass \
    -nhnv
